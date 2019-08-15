bf3130e25f3bbbd361dd1bafe005126e6e5d64b9
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-15 14:21:29 +0800

    remove unuse package

--- zuul/src/main/java/com/vector/cloud/service/zuul/filter/ErrorFilter.java
+++ zuul/src/main/java/com/vector/cloud/service/zuul/filter/ErrorFilter.java
@@ -6,7 +6,6 @@ package com.vector.cloud.service.zuul.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
